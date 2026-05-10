f27fdc34e8d6ffb18558426530438e942ba6b686
h.emami <Hossein.Emami@mahdaad.com>
2026-05-10 16:47:09 +0330

    feat: implement authentication business logic

---
+++ src/services/auth.service.ts
@@ -0,0 +1,84 @@
+import { prisma } from '../database/prisma'
+
+import {
+  hashPassword,
+  verifyPassword
+} from '../utils/hash'
+
+import { createAccessToken } from '../utils/token'
+
+type RegisterInput = {
+  email: string
+  password: string
+  fullName?: string
+}
+
+type LoginInput = {
+  email: string
+  password: string
+}
+
+export async function registerUser(data: RegisterInput) {
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: data.email
+    }
+  })
+
+  if (existingUser) {
+    throw new Error('Email already exists')
+  }
+
+  const hashedPassword = await hashPassword(
+    data.password
+  )
+
+  const user = await prisma.user.create({
+    data: {
+      email: data.email,
+      password: hashedPassword,
+      fullName: data.fullName
+    }
+  })
+
+  const token = createAccessToken({
+    sub: user.id,
+    email: user.email
+  })
+
+  return {
+    user,
+    token
+  }
+}
+
+export async function loginUser(data: LoginInput) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email
+    }
+  })
+
+  if (!user) {
+    throw new Error('Invalid credentials')
+  }
+
+  const isPasswordValid = await verifyPassword(
+    user.password,
+    data.password
+  )
+
+  if (!isPasswordValid) {
+    throw new Error('Invalid credentials')
+  }
+
+  const token = createAccessToken({
+    sub: user.id,
+    email: user.email
+  })
+
+  return {
+    user,
+    token
+  }
+}
