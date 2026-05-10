2b916bdf9f308bdebf22f837c07d4c87eb6930a7
h.emami <Hossein.Emami@mahdaad.com>
2026-05-10 16:37:56 +0330

    feat: configure express middlewares and routes

---
+++ app.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+import authRoutes from './routes/auth.routes'
+import linkRoutes from './routes/link.routes'
+
+export const app = express()
+
+app.use(cors())
+app.use(helmet())
+app.use(morgan('dev'))
+app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/links', linkRoutes)
