272c1336937fbae4c32853a1de8a1f5fe4cca232
h.emami <Hossein.Emami@mahdaad.com>
2026-05-11 10:36:25 +0330

    feat: implement link controllers

---
+++ src/controllers/link.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express'
+
+import {
+  createShortLink,
+  getLinkByCode,
+  removeLink
+} from '../services/link.service'
+
+export async function createLinkController(
+  req: Request,
+  res: Response
+) {
+  const link = await createShortLink({
+    originalUrl: req.body.originalUrl,
+    shortCode: req.body.shortCode,
+    userId: req.user!.sub
+  })
+
+  return res.status(201).json({
+    success: true,
+    data: link
+  })
+}
+
+export async function getLinkController(
+  req: Request,
+  res: Response
+) {
+  const link = await getLinkByCode(
+    req.params.shortCode
+  )
+
+  return res.json({
+    success: true,
+    data: link
+  })
+}
+
+export async function deleteLinkController(
+  req: Request,
+  res: Response
+) {
+  await removeLink(
+    req.params.shortCode,
+    req.user!.sub
+  )
+
+  return res.json({
+    success: true,
+    message: 'Link deleted successfully'
+  })
+}
