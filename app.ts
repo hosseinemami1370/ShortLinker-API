2f05155f0a1dde0d63fa6ece435f8aedd9593fbc
h.emami <Hossein.Emami@mahdaad.com>
2026-05-11 10:54:41 +0330

    refactor: register user routes and global error middleware

--- app.ts
+++ app.ts
@@ -5,13 +5,51 @@ import morgan from 'morgan'
 
 import authRoutes from './routes/auth.routes'
 import linkRoutes from './routes/link.routes'
+import userRoutes from './routes/user.routes'
+
+import { errorHandler } from './middlewares/error.middleware'
 
 export const app = express()
 
 app.use(cors())
+
 app.use(helmet())
+
 app.use(morgan('dev'))
+
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
+
 app.use('/api/links', linkRoutes)
+
+app.use('/api/users', userRoutes)
+
+app.use(errorHandler)import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+import authRoutes from './routes/auth.routes'
+import linkRoutes from './routes/link.routes'
+import userRoutes from './routes/user.routes'
+
+import { errorHandler } from './middlewares/error.middleware'
+
+export const app = express()
+
+app.use(cors())
+
+app.use(helmet())
+
+app.use(morgan('dev'))
+
+app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+
+app.use('/api/links', linkRoutes)
+
+app.use('/api/users', userRoutes)
+
+app.use(errorHandler)
